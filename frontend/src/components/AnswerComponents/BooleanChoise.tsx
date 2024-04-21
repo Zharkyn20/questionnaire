eccde47d1fb960b138ff20e4eae883925f760189
Muhammad <mdzhanov@gmail.com>
2024-04-21 14:42:39 +0600

    boolean test

--- frontend/src/components/AnswerComponents/BooleanChoise.tsx
+++ frontend/src/components/AnswerComponents/BooleanChoise.tsx
@@ -1,3 +1,27 @@
-const BooleanChoise = () => {}
+import { useState } from "react";
+import Button from "../shared/Button";
+import { BaseChoiseProps } from "./types";
+
+type BooleanChoiseProps = Omit<BaseChoiseProps, "options">;
+
+const BooleanChoise = ({ onChange }: BooleanChoiseProps) => {
+  const [selectedOption, setSelectedOption] = useState<boolean | undefined>();
+
+  const handleClick = (value: boolean) => {
+    setSelectedOption(value);
+    onChange(value);
+  };
+
+  return (
+    <div className="grid grid-cols-2 w-full gap-4 min-10">
+      <Button  selected={!!selectedOption} onClick={() => handleClick(true)}>
+        True
+      </Button>
+      <Button selected={selectedOption === false} onClick={() => handleClick(false)}>
+        False
+      </Button>
+    </div>
+  );
+};
 
 export default BooleanChoise;
