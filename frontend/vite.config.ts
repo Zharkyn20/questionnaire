c53ce0abbdd1a1c0c97380dd865a0b57d27eb7b4
Muhammad <mdzhanov@gmail.com>
2024-04-21 20:53:03 +0600

    connecting with backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,17 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
+      include: "**/*.svg",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
