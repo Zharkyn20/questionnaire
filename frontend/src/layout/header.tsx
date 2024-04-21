d4f183b5eefc0bf3447d1d4f9524651cc881333b
estebes <estebesm95@gmail.com>
2024-04-22 02:52:00 +0600

    added sign out

--- frontend/src/layout/header.tsx
+++ frontend/src/layout/header.tsx
@@ -1,10 +1,15 @@
+import Logout from "./logout";
+
 function Header() {
   return (
     <header>
-      <div className="container py-4">
-        <h2 className="text-3xl font-medium">
-          my <span className="text-primary">questionnaires</span>
-        </h2>
+      <div className="container py-4 shadow">
+        <div className="flex gap-4 justify-between items-center">
+          <h2 className="text-2xl md:text-3xl font-medium">
+            my <span className="text-primary">questionnaires</span>
+          </h2>
+          <Logout />
+        </div>
       </div>
     </header>
   );
