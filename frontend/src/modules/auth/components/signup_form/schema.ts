42678b06603c31573ab445827de7c50cd38d5582
estebes <estebesm95@gmail.com>
2024-04-21 19:57:11 +0600

    protected routes

--- frontend/src/modules/auth/components/signup_form/schema.ts
+++ frontend/src/modules/auth/components/signup_form/schema.ts
@@ -3,5 +3,5 @@ import * as yup from "yup";
 export const schema = yup.object({
   email: yup.string().required("Required").email("Invalid"),
   company_name: yup.string().required("Required"),
-  password: yup.string().required("Required").min(8),
+  password: yup.string().required("Required").min(5),
 });
