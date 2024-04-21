65943d34310eb8212ddcbca3d0102cfa72702aaa
zharkyn-askat <zharkyn-askat@pro-itech.ru>
2024-04-21 18:34:39 +0600

    Remove email veriofication logic

--- backend/app/routers/lms.py
+++ backend/app/routers/lms.py
@@ -11,32 +11,6 @@ from backend.app.services.verify_email import send_code_to_email, verify_verific
 router = APIRouter(prefix='/lms', tags=['LMS'])
 
 
-@router.post("/send_code/")
-async def send_code(email: str, db: Session = Depends(get_db_session)):
-    """
-    Send a code to the email.
-    """
-    user = db.query(LMS).filter(LMS.email == email).first()
-    if user:
-        raise HTTPException(status_code=400, detail="Email already exists")
-
-    is_email_sent = send_code_to_email(email)
-    if not is_email_sent:
-        raise HTTPException(status_code=500, detail="Failed to send code to email")
-    return {"message": "Code sent successfully"}
-
-
-@router.post("/verify_code/")
-async def verify_code(email: str, code: str):
-    """
-    Verify the code sent to the email.
-    """
-    is_code_verified = verify_verification_code(email, code)
-    if not is_code_verified:
-        raise HTTPException(status_code=400, detail="Invalid code")
-    return {"message": "Code verified successfully"}
-
-
 @router.post("/register/")
 async def register_user(email: str, name: str, password: str, db: Session = Depends(get_db_session)):
     """
@@ -47,14 +21,20 @@ async def register_user(email: str, name: str, password: str, db: Session = Depe
         raise HTTPException(status_code=400, detail="Email already exists")
 
     user = LMS(email=email, name=name)
-    is_email_verified = check_email_is_verified(email)
-    if not is_email_verified:
-        raise HTTPException(status_code=400, detail="Email is not verified")
 
     user.set_password(password)
     db.add(user)
     db.commit()
-    return {"message": "LMS registered successfully"}
+
+    access_token, access_token_lifetime = create_access_token(data={"sub": user.email})
+    refresh_token = create_refresh_token(data={"sub": user.email})
+
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+        "access_token_lifetime": access_token_lifetime
+    }
 
 
 @router.post("/login/")
