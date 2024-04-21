d2acdbe129128382ace2e8106f522a69da5e8fdf
estebes <estebesm95@gmail.com>
2024-04-22 01:16:00 +0600

    fixed upload file validation

---
+++ frontend/src/layout/header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <div className="container py-4">
+        <h2 className="text-3xl font-medium">
+          my <span className="text-primary">questionnaires</span>
+        </h2>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
