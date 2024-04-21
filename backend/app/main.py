6161f1caf2e285fcd34ea3c70b307ae974977810
zharkyn-askat <zharkyn-askat@pro-itech.ru>
2024-04-21 11:50:28 +0600

    Auth Feature

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
-from const import (
+
+from backend.app.backend.config import Base, engine
+from backend.app.const import (
     OPEN_API_DESCRIPTION,
     OPEN_API_TITLE,
 )
 
-from routers import course
+from backend.app.routers import course, lms
+
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title=OPEN_API_TITLE,
@@ -18,5 +23,4 @@ async def root():
 
 
 app.include_router(course.router)
-
-
+app.include_router(lms.router)
