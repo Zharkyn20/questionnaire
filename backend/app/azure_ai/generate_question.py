4392b44700695d74e04dde6e8fd03f26605e4a6d
EMIRABYBEKOV <emir.abdybekov@bk.ru>
2024-04-21 16:51:28 +0200

    work in progress

--- backend/app/azure_ai/generate_question.py
+++ backend/app/azure_ai/generate_question.py
@@ -5,18 +5,21 @@ from sqlalchemy.orm import Session
 from fastapi import Depends
 
 # genearte questions for subtopic(now simple version)
-def generate_question(amount, subtopic_id, description, session: Session = Depends(get_db_session)):
+def generate_question(amount, subtopic_id, description, user_id, session: Session = Depends(get_db_session)):
     # Logik with azure AI to generate questions
 
     for i in range(amount):
         question = Question(
             number=i,
+            answered=False,
             subtopic_id=subtopic_id,
             question=f"my question with number {i} is ->  what is ypu name? (correct answer is 19)",
             type="single",
             answer="19",
             time=9,
-            answers=["20", "90", "19", "99"]
+            variants=["20", "90", "19", "99"],
+            user_id=user_id,
+            answers=None,
         )
         session.add(question)
         session.commit()
