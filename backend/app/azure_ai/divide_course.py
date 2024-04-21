9ab11459dfbd861104e2ab800d34631aea94d411
zharkyn-askat <zharkyn-askat@pro-itech.ru>
2024-04-21 22:33:11 +0600

    Add topic division

--- backend/app/azure_ai/divide_course.py
+++ backend/app/azure_ai/divide_course.py
@@ -1,27 +1,46 @@
 from backend.app.chatgpt.controller import gpt
 
 
-def divide_course(course):
+def divide_course(course, course_content):
     print("course", course)
 
     response = gpt.generate_chat_completion(
         messages=[
-            {"role": "system", "content": "You are a virtual assistant, who analyzes the course content and divides it into topics with contents, without updating/deleting."},
-            {"role": "user", "content": course.file_content}
+            {"role": "system", "content": "You are a virtual assistant, who splits the course content into topics without changing topic description. The response should be a list of objects (number, title, content)"},
+            {"role": "user", "content": course_content}
         ],
-
     )
-    print("response", response.choices[0].message)
 
-    return [
-        {
-            "title": "Addition",
-            "description": "description example",
+    print("response", response)
+    print("response.content", response.content)\
+
+    parsed_content = parse_content(response.content)
+    list_of_subtopics = []
+    for content in parsed_content:
+        list_of_subtopics.append({
+            "title": content["title"],
+            "description": content["description"],
             "course_id": course.id,
-        },
-        {
-            "title": "Substruction",
-            "description": "description example",
-            "course_id": course.id,
-        }
-    ]
+        })
+
+    return list_of_subtopics
+
+
+def parse_content(content):
+    sections = content.split("\n\n")
+    parsed_sections = []
+    for section in sections:
+        if section.strip():
+            # Extract title
+            title_start_index = section.find(" ")
+            title = section[:title_start_index].strip()
+
+            # Extract content
+            content_start_index = section.find("\n") + len("\n")
+            content_text = section[content_start_index:].strip()
+
+            parsed_sections.append({
+                "title": title,
+                "description": content_text,
+            })
+    return parsed_sections
